b0f1a3b03bca6475cde57e6710da86e84163c1e5
bmbrost <bmbrost@gmail.com>
2015-05-29 12:17:41 -0600

    Start DP mixture simulation code

---
+++ dirichlet.process.mixture.sim.R
@@ -0,0 +1,54 @@
+###
+### Simulation 1-dimensional Dirichlet process mixture
+###
+
+# Using blocked Gibbs sampler per Gelman et al. 2014, section 23.2
+a0 <- 0.5  # concentration parameter
+K <- 100  # total number of clusters 
+P0 <- seq(1,K,1)  # base probability measure; discrete uniform distribution 
+N <- 20  # maximum number of clusters
+
+# Generate data according to stick-breaking process
+v <- rbeta(N,1,a0)
+v.comp <- 1-v
+pi.h <- c(v*c(1,cumprod(v.comp[-N])),0)
+
+k <- sample(P0,N+1,replace=FALSE)
+y <- sample(k,n,replace=TRUE,prob=pi.h)
+hist(y,breaks=0:K)
+
+
+# Generate data according to Chinese restaurant process (Gelman et al. 2014, section 23.2)
+a0 <- 0.1  # concentration parameter
+K <- 100  # total number of clusters 
+P0 <- seq(1,K,1)  # base probability measure; discrete uniform distribution 
+N <- 20  # maximum number of clusters
+
+n <- 100
+k <- numeric(n)
+
+k[1] <- sample(P0,1)
+for (i in 2:n) {
+  tab <- table(k)
+  idx <- as.numeric(names(tab))>0
+  occ <- as.numeric(names(tab)[idx])
+  p <- rep(a0/(a0+n-1),100)
+  p[occ] <- tab[idx]/(a0+n-1)
+  k[i] <- sample(P0,1,prob=p)
+}
+
+hist(k,breaks=0:n)
+
+
+###
+### Fit model
+###
+
+source("/Users/brost/Documents/git/Haulouts/dirichlet.process.prior.mcmc.R")
+out1 <- dirichlet.process.prior.mcmc(k,P0,priors=list(a=0.01,b=0.1),tune=list(a0=0.1),
+   start=list(a0=a0),n.mcmc=1000)
+tab <- hist(k,breaks=0:n,plot=FALSE)
+boxplot(c(out1$P)*n~rep(1:n,out1$n.mcmc),pch=19,cex=0.25)
+points(1:n,tab$counts,col="red")
+points(1:n,apply(out1$P*n,1,mean),col="blue")
+
