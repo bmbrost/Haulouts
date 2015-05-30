0afb8466b57f96725ac194def1ddfd9fbf2899f5
bmbrost <bmbrost@gmail.com>
2015-05-29 22:38:54 -0600

    Update to stick-breaking construction

--- dirichlet.process.prior.sim.R
+++ dirichlet.process.prior.sim.R
@@ -2,37 +2,31 @@
 ### Simulation 1-dimensional Dirichlet process prior (Gelman et al. 2014, section 23.2)
 ###
 
-a0 <- 0.1  # concentration parameter
-P0 <- seq(0,6,0.0001)  # base probability measure
+a0 <- 20  # concentration parameter
+P0 <- seq(0,6,1)  # base probability measure
 n <- 100  # number of observations
 
-# Generate data according to Chinese restaurant process
+# Generate data according to stick-breaking process
 
 # Discrete uniform base distribution
-k <- numeric(n)
-k[1] <- sample(P0,1)
-for (i in 2:n) {  
-  tab <- table(k)
-  idx <- as.numeric(names(tab))>0
-  occ <- as.numeric(names(tab)[idx])+1
-  p <- rep(a0/(a0+n-1),length(P0))
-  p[occ] <- tab[idx]/(a0+n-1)
-  k[i] <- sample(P0,1,prob=p)
-}
-hist(k,breaks=1000)
+N <- length(P0)  # maximum number of clusters
+v <- c(rbeta(N-1,1,a0),1)
+pie <- v*c(1,cumprod((1-v[-N])))
+
+theta <- sample(P0,N,replace=FALSE)  # clusters randomly drawn from P0
+k <- sample(theta,n,replace=TRUE,prob=pie)  # cluster assignments of observations
+hist(k,breaks=P0)
 k.tab <- table(k)
 
 # Continuous uniform base distribution
-k <- numeric(n)
-k[1] <- runif(1,min(P0),max(P0))
-for (i in 2:n) {
-  tab <- table(k[k>0])
-  p <- tab/(a0+n-1)
-  p <- c(p,1-sum(p))
-  k.idx <- sample(1:length(p),1,prob=p)
-  k[i] <- ifelse(k.idx==length(p),runif(1,min(P0),max(P0)),as.numeric(names(tab)[k.idx]))
-}
+N <- 20  # maximum number of clusters for truncation approximation to DPM
+v <- c(rbeta(N-1,1,a0),1)
+pie <- v*c(1,cumprod((1-v[-N])))
+
+theta <- runif(N,min(P0),max(P0))  # clusters randomly drawn from P0
+k <- sample(theta,n,replace=TRUE,prob=pie)  # cluster assignments of observations
 hist(k,breaks=1000)
+# hist(k,breaks=P0)  # for comparison to discrete base distribution
 k.tab <- table(k)
 
 
@@ -42,10 +36,13 @@ k.tab <- table(k)
 
 source("/Users/bmb/Documents/git/Haulouts/dirichlet.process.prior.mcmc.R")
 source("/Users/brost/Documents/git/Haulouts/dirichlet.process.prior.mcmc.R")
-out1 <- dirichlet.process.prior.mcmc(k,P0,priors=list(a=0.01,b=0.1),tune=list(a0=0.1),
-   start=list(a0=a0),n.mcmc=1000)
+hist(rgamma(10000,2,2),breaks=100)
+out1 <- dirichlet.process.prior.mcmc(k,P0,priors=list(a=2,b=2),tune=list(a0=1.5),
+   start=list(a0=a0),n.mcmc=10000)
 # boxplot(c(out1$P)*n~rep(as.numeric(names(k.tab)),out1$n.mcmc),pch=19,cex=0.25)
 plot(rep(as.numeric(names(k.tab)),out1$n.mcmc),c(out1$P)*n,pch=19,cex=0.25,col=rgb(0,0,0,0.05))
 points(as.numeric(names(k.tab)),k.tab,col="red")
 points(as.numeric(names(k.tab)),apply(out1$P*n,1,mean),col="blue")
 
+hist(out1$a0);abline(v=a0,col=2,lty=2)
+mean(out1$a0)
