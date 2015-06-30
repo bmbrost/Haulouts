f2796a06568e1a78268db2cea00e0908ac4010c2
bmbrost <bmbrost@gmail.com>
2015-06-30 12:36:31 -0600

    Start DP mixture haulout model simulation

---
+++ haulout.dp.mixture.sim.R
@@ -0,0 +1,132 @@
+rm(list=ls())
+
+# library(cluster)
+# library(msm)
+# library(pscl)
+library(MCMCpack)  # for rdirichlet(...)
+
+###
+### Simulate 2-dimensional haul out data using a Dirichlet process mixture 
+###
+
+# Define 2-dimensional rectangular support for mu, the true harbor seal locations
+S.bar <- cbind(c(-10,-2,-2,-10,-10),c(0,0,20,20,0))  # complement of S, i.e., the land domain
+S.tilde <- cbind(c(max(S.bar[,1]),2,2,max(S.bar[,1]),max(S.bar[,1])),S.bar[,2])  # support 
+	# of haul out process
+S <- cbind(c(max(S.bar[,1]),10,10,max(S.bar[,1]),max(S.bar[,1])),S.bar[,2])  # support of
+	# movement process (marine and haul-out environments)
+
+# Simulate cluster locations and assignments using stick-breaking process 
+# See Ishwaran and James (2001), Gelman et al. (2014), Section 23.2
+T <- 500  # number of locations to simulate
+a0 <- 1.5  # concentration parameter
+H <- 50  # maximum number of clusters for truncation approximation
+
+v <- c(rbeta(H-1,1,a0),1)
+pie <- v*c(1,cumprod((1-v[-H])))
+theta <- cbind(runif(H,min(S.tilde[,1]),max(S.tilde[,1])),
+	runif(H,min(S.tilde[,2]),max(S.tilde[,2])))  # clusters randomly drawn from P0
+h <- sample(1:H,T,replace=TRUE,prob=pie)  # latent cluster assignments
+h <- theta[h,]  # latent clusters
+
+
+p <- 0.5  # probability of being on the haul-out
+z <- rbinom(T,1,p)  # haulout indicator variable
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+P0 <- cbind(c(-2,2,2,-2,-2),c(-2,-2,2,2,-2))  # 2-D uniform base probability measure
+
+sim1 <- stick.2d(n,P0,a0,H)
+z <- sim1$z  # cluster assignments
+z.tab <- table(z)
+
+# Generate observations conditional on cluster assignments
+sigma <- 0.1
+y <- matrix(rnorm(n*2,z,sigma),,2)
+
+b <- 3*c(-sigma,sigma) # Plot buffer for errors
+plot(0,0,xlim=range(P0[,1])+b,ylim=range(P0[,2])+b,pch="",yaxt="n",xaxt="n",xlab="",ylab="")
+polygon(x=P0[,1],y=P0[,2],col="gray85")
+points(y,pch=19,cex=0.5)
+points(z,pch=19,cex=1,col=rgb(1,0,0,0.15))
+
+
+###
+### Fit models
+###
+
+# Fit model using blocked Gibbs sampler 
+source("/Users/brost/Documents/git/DPMixtures/dp.mixture.blocked.2d.mcmc.R")
+# hist(rgamma(1000,2,2),breaks=100)
+# hist(rgamma(1000,1,1),breaks=100)
+start <- list(a0=a0,z=z,#z=fitted(kmeans(y,rpois(1,10))),
+  sigma=sigma,pie=rdirichlet(1,rep(1/H,H))) #sim1$pie)  # 
+out1 <- dpmixture.blocked.2d.mcmc(y,P0,
+  priors=list(H=H,r=20,q=10,sigma.l=0,sigma.u=5),
+  tune=list(z=0.5,sigma=0.01),start=start,n.mcmc=10000)
+
+mod <- out1
+idx <- 1:100
+idx <- 1:1000
+idx <- 1:2500
+idx <- 1:10000
+
+# True clusters
+b <- 3*c(-sigma,sigma) # Plot buffer for errors
+plot(0,0,xlim=range(P0[,1])+b,ylim=range(P0[,2])+b,pch="",yaxt="n",xaxt="n",xlab="",ylab="")
+polygon(x=P0[,1],y=P0[,2],col="gray85")
+points(mod$z[,1,idx],mod$z[,2,idx],pch=19,cex=0.5,col=rgb(0,0,0,0.15))
+points(y,pch=19,cex=0.2,col=3)
+points(z,pch=19,cex=0.5,col=rgb(1,0,0,1))
+
+cl <- kmeans(apply(mod$z,2,I),11)
+points(cl$centers,col=4,pch=19)
+
+
+# Concentration parameter
+hist(mod$a0[idx],breaks=100);abline(v=a0,col=2,lty=2) 
+mean(mod$a0[idx])*log(n)
+
+# Observation error
+hist(mod$sigma[idx],breaks=100);abline(v=sigma,col=2,lty=2)
+
+# Modeled number of clusters
+nclust <- apply(mod$z[,,idx],c(3),function(x) nrow(unique(x)))
+head(nclust)
+plot(nclust,type="l")
+abline(h=nrow(unique(z)),col=2,lty=2)  # true number of clusters  
+
+barplot(table(nclust))
+
+
+
+
+plot(apply(mod$z[,idx],2,max),type="l")
+cl.ranks <- apply(mod$z[,idx],2,dense_rank)
+plot(c(mod$z[,idx])[c(cl.ranks)==8],type="l")
+
+hist(c(mod$z[,idx])[c(test)==1],breaks=500,xlim=range(mod$z[idx]),ylim=c(0,5),prob=TRUE)  
+hist(c(mod$z[,idx])[c(test)==5],col=5,breaks=500,add=TRUE,prob=TRUE)  
+
+pt.idx <- 94
+plot(mod$z[pt.idx,idx],type="l");abline(h=z[pt.idx],col="red",lty=2)
+hist(mod$z[,idx],breaks=5000,xlim=c(range(mod$z[pt.idx,])+c(-10,10)),prob=TRUE)
+hist(mod$z[pt.idx,idx],breaks=50,col="red",add=TRUE,border="red",prob=TRUE);abline(v=z[pt.idx],col="red",lty=2)
+points(y[pt.idx],-0.010,pch=19)
+
+which.max(apply(mod$z,1,var))
+abline(v=29.75)
+which.min(sapply(y,function(x) dist(c(x,29.75))))
