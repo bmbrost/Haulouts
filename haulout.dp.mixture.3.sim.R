8d176a3e8e3d685da027a92e171035aeb7a72d54
bmbrost <bmbrost@gmail.com>
2015-08-24 17:57:12 -0600

    Minor revisions

--- haulout.dp.mixture.3.sim.R
+++ haulout.dp.mixture.3.sim.R
@@ -14,7 +14,7 @@ library(splines)
 ### Define 2-dimensional rectangular support for mu, the true harbor seal locations
 ###
 
-S.bar <- cbind(c(-10,-0.5,-0.5,-10,-10),c(0,0,20,20,0))  # complement of S, i.e., the land domain
+S.bar <- cbind(c(-10,-0.5,-0.5,-10,-10),c(0,0,10,10,0))  # complement of S, i.e., the land domain
 n.haulout <- 100
 S.tilde <- cbind(max(S.bar[,1]),  # support of haul out process
 	seq(min(S.bar[,2]),max(S.bar[,2]),length.out=n.haulout)) 
@@ -27,23 +27,29 @@ S <- cbind(c(max(S.bar[,1]),10,10,max(S.bar[,1]),max(S.bar[,1])),S.bar[,2])  # s
 ###
 
 T <- 500  # number of locations to simulate
-theta <- 1.0  # concentration parameter
+theta <- 2.0  # concentration parameter
 H <- 25  # maximum number of clusters for truncation approximation
 
 # Resource selection strength for haul-out sites
-gamma <- c(0.25,0.1,-1)  # Haul-out site selection coefficients
 U <- cbind(1,S.tilde[,2],sin(S.tilde[,2]))  # design matrix for haul-out site RSF
+U[,-1] <- scale(U[,-1])
+gamma <- c(-1,0.025,0.5)  # Haul-out site selection coefficients
+
+U <- cbind(1,sin(5*S.tilde[,2]))  # design matrix for haul-out site RSF
+U[,-1] <- scale(U[,-1])
+gamma <- c(2,8)  # Haul-out site selection coefficients
+
 qU <- ncol(U)
 selection <- exp(U%*%gamma)
-plot(selection)
+plot(selection,type="l")
 
 mu.0 <- sample(1:n.haulout,H,prob=selection)
 mu.0 <- S.tilde[mu.0,]
 length(unique(mu.0[,2]))==H
 eta <- c(rbeta(H-1,1,theta),1)  # stick-breaking weights
 pie <- eta*c(1,cumprod((1-eta[-H])))  # probability mass
-h.idx <- sample(1:H,T,replace=TRUE,prob=pie)  # latent cluster assignments
-h <- mu.0[h.idx,]  # latent clusters
+h.match <- sample(1:H,T,replace=TRUE,prob=pie)  # latent cluster assignments
+h <- mu.0[h.match,]  # latent clusters
 
 plot(0,0,xlim=c(min(S.bar[,1]),max(S[,1])),ylim=range(S.bar[,2]),
 	pch="",yaxt="n",xaxt="n",xlab="",ylab="")
@@ -105,7 +111,7 @@ qW <- ncol(W)
 ###
 
 # Simulate true locations
-sigma.mu <- 2  # dispersion about haul-out for at-sea locations
+sigma.mu <- 1  # dispersion about haul-out for at-sea locations
 mu <- matrix(0,T,2)
 mu[z==1,] <- h[z==1,]
 mu[z==0,] <- cbind(rtnorm(T-sum(z),h[z==0,1],sigma.mu,lower=S[1,1],upper=S[2,1]),
@@ -139,18 +145,19 @@ source("/Users/brost/Documents/git/haulouts/haulout.dp.mixture.3.mcmc.R")
 start <- list(theta=theta,h=h,z=z,p=p,#h=fitted(kmeans(s,rpois(1,10))),
   sigma=sigma,sigma.mu=sigma.mu,pie=pie,beta=beta,gamma=gamma)  # rdirichlet(1,rep(1/H,H))) 
 priors <- list(H=H,r=4,q=2,sigma.l=0,sigma.u=5,sigma.mu.l=0,sigma.mu.u=5,
-	alpha=1,beta=1,sigma.beta=1,mu.gamma=rep(0,qU),sigma.gamma=1)
-tune <- list(sigma=0.05,sigma.mu=0.25,mu.0=1)
+	sigma.beta=5,mu.gamma=rep(0,qU),sigma.gamma=5)
+tune <- list(sigma=0.05,sigma.mu=0.2,mu.0=2,gamma=3)
 # hist(rgamma(1000,4,2))
 # hist(rgamma(1000,5,2.5))
 out1 <- haulout.dpmixture.3.mcmc(s,y,X[s.idx,],X[-s.idx,],W[s.idx,],W[-s.idx,],
 	S,S.tilde,U,sigma.alpha=2,priors=priors,tune=tune,start=start,n.mcmc=2000)
 
-mod <- out1 
+mod <- out1
 # idx <- 1:100
 idx <- 1:1000
 idx <- 1:2000
 idx <- 1:10000
+idx <- 1:20000
 
 # True clusters
 b <- 3*c(-sigma,sigma) # Plot buffer for errors
@@ -166,10 +173,11 @@ plot(jitter(mod$h[,1,idx]),jitter(mod$h[,2,idx]),
 	pch=19,cex=0.5,col=rgb(0,0,0,0.0025),asp=TRUE,ylim=range(S.tilde))
 points(h,pch=1,cex=1,col=rgb(1,0,0,1))
 points(s,pch=19,cex=0.2,col=3)
-points(S.tilde+cbind(rep(-1.5,n.haulout),0),cex=selection/max(selection))
+points(S.tilde+cbind(rep(-1.5,n.haulout),0),cex=selection/max(selection),pch=19)
 
-pt.idx <- 27
-points(mod$h[pt.idx,1,idx],mod$h[pt.idx,2,idx],pch=19,cex=0.2,col=rgb(1,1,0,0.25))
+which(s[,1] < (-1.5))
+pt.idx <- 280
+points(mod$h[pt.idx,1,idx],mod$h[pt.idx,2,idx],pch=19,cex=0.2,col=rgb(1,1,0,0.5))
 points(mu[pt.idx,1],mu[pt.idx,2],pch=19,cex=0.75)
 points(s[pt.idx,1],s[pt.idx,2],pch=19,col=2)
 table(mod$z[pt.idx,idx])
@@ -184,6 +192,16 @@ abline(h=0,col=2,lty=2)
 segments(1:qX,beta.quant[,1],1:qX,beta.quant[,2],col="lightgrey")
 points(beta.hat,pch=19,col=rgb(0,0,0,0.25))
 points(beta,pch=19)
+
+# Haul-out selection covariates
+matplot(mod$gamma[idx,],type="l"); abline(h=gamma,col=1:3,lty=2) 
+gamma.hat <- apply(out1$gamma[idx,],2,mean)
+gamma.quant <- t(apply(out1$gamma[idx,],2,quantile,c(0.025,0.975)))
+plot(gamma.hat,pch=19,col=rgb(0,0,0,0.25),ylim=c(range(gamma.quant)))
+abline(h=0,col=2,lty=2)
+segments(1:qU,gamma.quant[,1],1:qU,gamma.quant[,2],col="lightgrey")
+points(gamma.hat,pch=19,col=rgb(0,0,0,0.25))
+points(gamma,pch=19)
 
 # Inference on alpha
 alpha.hat <- apply(out1$alpha[idx,],2,mean)
@@ -200,19 +218,14 @@ plot(alpha.hat,pch=19,col=rgb(0,0,0,0.25),ylim=c(range(alpha.quant)))
 lines(alpha.hat,col=rgb(0,0,0,0.25))
 abline(h=0,col=2,lty=2)
 
-# Haul-out selection covariates
-matplot(mod$gamma[idx,],type="l"); abline(h=gamma,col=1:3,lty=2) 
-gamma.hat <- apply(out1$gamma[idx,],2,mean)
-gamma.quant <- t(apply(out1$gamma[idx,],2,quantile,c(0.025,0.975)))
-plot(gamma.hat,pch=19,col=rgb(0,0,0,0.25),ylim=c(range(gamma.quant)))
-abline(h=0,col=2,lty=2)
-segments(1:qU,gamma.quant[,1],1:qU,gamma.quant[,2],col="lightgrey")
-points(gamma.hat,pch=19,col=rgb(0,0,0,0.25))
-points(gamma,pch=19)
-
-# Concentration parameter
+# Concentration parameter (theta) and number of clusters
 hist(mod$theta[idx],breaks=100);abline(v=theta,col=2,lty=2) 
 mean(mod$theta[idx])*log(T)
+
+# n.cls <- apply(mod$h[,,idx],3,function(x) nrow(unique(x)))
+plot(mod$n.cls,type="l")
+abline(h=nrow(unique(h)),col=2,lty=2)  # true number of clusters  
+barplot(table(mod$n.cls)) 
 
 # Observation error
 hist(mod$sigma[idx],breaks=100);abline(v=sigma,col=2,lty=2)
@@ -228,11 +241,12 @@ mean(mod$sigma.mu[idx])
 z.hat <- apply(mod$z[,idx],1,sum)/(length(idx))
 boxplot(z.hat~z,ylim=c(0,1))
 plot(s[,1],z.hat,ylim=c(0,1),col=z+1)
+abline(v=S.tilde[1,1],col=3,lty=2)
 plot(z,col=3,pch=19,cex=0.5)
 points(z.hat,pch=19,cex=0.5,col=rgb(0,0,0,0.25))
 
-v <- apply(out1$beta[idx,],1,function(x) X[s.idx,]%*%x)+
-	apply(out1$alpha[idx,],1,function(x) W[s.idx,]%*%x)
+v <- apply(mod$beta[idx,],1,function(x) X[s.idx,]%*%x)+
+	apply(mod$alpha[idx,],1,function(x) W[s.idx,]%*%x)
 v.inv <- matrix(pnorm(v),,T,byrow=TRUE)
 v.inv.mean <- apply(v.inv,2,mean)
 plot(v.inv.mean,z.hat)
@@ -241,20 +255,14 @@ plot(v.inv.mean,pch=19,col=rgb(0,0,0,0.25),ylim=c(0,1))
 segments(1:T,v.inv.quant[,1],1:T,v.inv.quant[,2],col=rgb(0,0,0,0.15))
 abline(h=0.5,col=2,lty=2)
 points(z,col=3,pch=19,cex=0.5)
-points(z.hat,pch=19,cex=0.25)
+points(z.hat,pch=19,cex=0.25,col=1)
 
 # Inference for y
-v.tilde.inv <- matrix(pnorm(out1$v[,idx]),,T,byrow=TRUE)
+v.tilde.inv <- matrix(pnorm(mod$v[,idx]),,T,byrow=TRUE)
 v.tilde.inv.mean <- apply(v.tilde.inv,2,mean)
 v.tilde.inv.quant <- t(apply(v.tilde.inv,2,quantile,c(0.025,0.975)))
 plot(v.tilde.inv.mean,pch=19,col=rgb(0,0,0,0.25),ylim=c(0,1))
 segments(1:T,v.tilde.inv.quant[,1],1:T,v.tilde.inv.quant[,2],col=rgb(0,0,0,0.15))
 points(y,col=4,pch=19,cex=0.5)
-
-# Modeled number of clusters
-# n.cls <- apply(mod$h[,,idx],3,function(x) nrow(unique(x)))
-plot(mod$n.cls,type="l")
-abline(h=nrow(unique(h)),col=2,lty=2)  # true number of clusters  
-barplot(table(mod$n.cls)) 
 
 
