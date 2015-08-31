d0187690b0ebfbc3c63142041a7d6798417a8de6
bmbrost <bmbrost@gmail.com>
2015-08-31 16:58:54 -0600

    Start model implementation for real harbor seal data

---
+++ haulouts.mcmc.R
@@ -0,0 +1,377 @@
+haulouts.1.mcmc <- function(s,y,X,X.tilde,W,W.tilde,S.tilde,sigma.alpha,
+	priors,tune,start,n.mcmc,n.cores=NULL){
+ 
+ 	###
+ 	### Brian M. Brost (13 AUG 2015)
+ 	### See haulout.dpmixture.2.sim.R to simulate data according to this model specification,
+ 	### and haulout.dp.mixture.2.pdf for the model description, model statement, and
+ 	### full conditional distributions
+ 	###
+ 	
+ 	###
+ 	### Function arguments: s=telemetry locations, y=ancillary data source containing
+ 	### binary wet/dry status; X=design matrix containing covariates influencing wet/dry
+ 	### status of telemetry locations s; X.tilde=design matrix containing covariates
+ 	### influencing wet/dry status of ancillary data y; W=basis expansion for s; 
+ 	### W.tilde=basis expansion for y; S.tilde=support Dirichlet process mixture 
+ 	### (i.e., haul-out sites); sigma.alpha=standard deviation of parameter 
+ 	### model for 'random' effects
+ 	###
+ 
+ 
+	t.start <- Sys.time()
+	
+
+	###
+	### Libraries and Subroutines
+	###
+  
+	# library(MCMCpack)  # for Dirichlet distribution functions
+	library(data.table)  # for tabulating and summing
+	# library(dplyr)  # dense_rank() for ranking clusters smallest to largest
+	library(doParallel)  # for parallel processing
+	library(foreach)  # for parallel processing  
+	# library(mvtnorm)  # for multivariate normal density
+	# library(msm)  # for truncated normal density
+  
+	get.mu.0 <- function(x,h.idx,z,s,Sigma.inv,Sigma.mu.inv){
+		# browser()
+		idx.0 <- which(h.idx==x&z==0)
+		idx.1 <- which(h.idx==x&z==1)
+		n.0 <- length(idx.0)
+		n.1 <- length(idx.1)
+		b <- colSums(s[idx.1,]%*%Sigma.inv)+colSums(s[idx.0,]%*%(Sigma.inv+Sigma.mu.inv))
+		A.inv <- solve(n.1*Sigma.inv+n.0*(Sigma.inv+Sigma.mu.inv))
+		rnorm(2,A.inv%*%b,sqrt(diag(A.inv)))	# proposal for mu.0	
+	}
+
+	truncnormsamp <- function(mu,sig2,low,high,nsamp){
+		flow <- pnorm(low,mu,sqrt(sig2)) 
+		fhigh <- pnorm(high,mu,sqrt(sig2)) 
+		u <- runif(nsamp) 
+		tmp <- flow+u*(fhigh-flow)
+		x <- qnorm(tmp,mu,sqrt(sig2))
+		x
+	}
+	
+	###
+	###  Create cluster for parallel processing
+	###
+  
+	# if(is.null(n.cores)) n.cores <- detectCores() - 1
+	# if(n.cores==1) registerDoSEQ() else registerDoParallel(cores=n.cores) # multicore 	functionality	
+	# mcoptions <- list(preschedule=TRUE)
+	# cat(paste("\nUsing",n.cores,"cores for parallel processing."))
+  
+	###
+	###  Setup Variables 
+	###
+  
+	# browser() 
+	T <- nrow(s)  # number of telemetry locations
+	n <- length(y)  # number of wet/dry observations
+	qX <- ncol(X)
+	qW <- ncol(W)
+	v <- numeric(n+T)  # auxilliary variable for continuous haul-out process
+	X.comb <- rbind(X,X.tilde)  # combined design matrix for updates on alpha, beta
+	W.comb <- rbind(W,W.tilde)  # combined design matrix for updates on alpha, beta 
+
+	###
+	### Starting values and priors
+	###
+# browser() 
+	beta <- matrix(start$beta,qX)
+	alpha <- matrix(0,qW)
+	# alpha <- start$alpha
+	a0 <- start$a0
+	sigma <- start$sigma
+	sigma.mu <- start$sigma.mu
+	pie <- start$pie
+	z <- start$z
+	H <- priors$H
+	
+	Sigma.inv <- solve(sigma^2*diag(2))
+	Sigma.mu.inv <- solve(sigma.mu^2*diag(2))
+  	mu.beta <- matrix(0,qX,1)
+	Sigma.beta <- diag(qX)*priors$sigma.beta^2
+	Sigma.beta.inv <- solve(Sigma.beta)
+	mu.alpha <- matrix(0,qW,1)
+	Sigma.alpha <- diag(qW)*sigma.alpha^2
+  	Sigma.alpha.inv <- solve(Sigma.alpha)
+	sd.tmp <- sqrt(sigma^2+sigma.mu^2)
+
+	y1 <- which(y==1)+T
+	y0 <- which(y==0)+T
+	y1.sum <- length(y1)
+	y0.sum <- length(y0)
+	linpred <- X.comb%*%beta+W.comb%*%alpha
+
+	###
+	### Set up Dirichlet process mixture variables
+	###
+
+	mu.0 <- unique(h)  # unique cluster location s
+	n.cls <- nrow(mu.0)  # number of clusters
+	h.idx <- c(1:n.cls)[match(start$h[,1],mu.0[,1])]  # cluster membership indicator
+
+	# Tabulate cluster membership with data.table and setdiff	
+	# dt.h.idx <- as.data.table(h.idx)
+	# dt.tab.cls <- dt.h.idx[,.N,by=h.idx]
+	# setkey(dt.tab.cls,N)
+	# idx.cls <- rev(dt.tab.cls[,h.idx])
+	# samp.cls <- c(idx.cls,setdiff(1:H,idx.cls))  # order in which clusters are sampled
+
+	# Tabulate cluster membership with data.table but not setdiff
+	# h.idx <- c(h.idx,1:H)
+	# dt.h.idx <- as.data.table(h.idx)
+	# dt.tab.cls <- dt.h.idx[,.N,by=h.idx]
+	# dt.tab.cls[,N:=N-1]
+	# setkey(dt.tab.cls,N)
+	# idx.cls <- rev(dt.tab.cls[N>0,h.idx])
+	# samp.cls <- rev(dt.tab.cls[,h.idx])  # order in which clusters are sampled
+
+	# Tabulate cluster membership with base functions
+	tab.cls <- table(h.idx)  # tabulate cluster membership
+	ord <- order(tab.cls,decreasing=TRUE) # order of clusters by membership
+	tab.cls <- tab.cls[ord]  # ordered largest to smallest
+	idx.cls <- as.numeric(names(tab.cls))  # 'occupied' clusters in order
+	samp.cls <- c(idx.cls,setdiff(1:H,idx.cls))  # order in which clusters are sampled
+ 
+	# Propose values for mu.0
+	n.cls.star <- H-n.cls  # number of new clusters to propose
+	mu.0 <- rbind(mu.0, cbind(runif(n.cls.star,S.tilde[1,1],S.tilde[2,1]),
+      runif(n.cls.star,S.tilde[1,2],S.tilde[3,2])))  # update mu.0 with mu.star
+
+  	###
+	### Create receptacles for output
+	###
+  
+	h.idx.save <- matrix(0,T,n.mcmc)  # cluster assignment indicator variable
+	h.save <- array(0,dim=c(T,2,n.mcmc))  # cluster assignment indicator variable
+	a0.save <- numeric(n.mcmc)  # concentration parameter
+	mu.0.save <- array(0,dim=c(H,2,n.mcmc))  # cluster locations
+	n.cls.save <- numeric(n.mcmc)
+	z.save <- matrix(0,T,n.mcmc)  # haul-out indicator variable
+	alpha.save <- matrix(0,n.mcmc,qW)
+	beta.save <- matrix(0,n.mcmc,qX)
+	v.save <- matrix(0,T+n,n.mcmc)
+	sigma.save <- numeric(n.mcmc)  # telemetry measurement error
+	sigma.mu.save <- numeric(n.mcmc)  # dispersion about haul-out site
+	# sigma.alpha.save <- numeric(n.mcmc)  # standard deviation of parameter model
+	D.bar.save <- numeric(n.mcmc)  # D.bar for DIC calculation
+
+	keep <- list(sigma=0,sigma.mu=0)
+    
+	###
+	### Begin MCMC loop
+	###
+  
+	for (k in 1:n.mcmc) {
+    	if(k%%1000==0) cat(k,"");flush.console()
+
+		###
+		### Updates pertaining to wet/dry status of y and z
+		###
+# browser()
+			# Sample v(t.tilde) (auxilliary variable for y) 
+		  	v[y1] <- truncnormsamp(linpred[y1],1,0,Inf,y1.sum)
+		  	v[y0] <- truncnormsamp(linpred[y0],1,-Inf,0,y0.sum)
+	
+			# Sample v(t) (auxilliary variable for s) 
+			z1 <- which(z==1)
+			z0 <- which(z==0)
+			z1.sum <- length(z1)
+			z0.sum <- length(z0)
+		  	v[z1] <- truncnormsamp(linpred[z1],1,0,Inf,z1.sum)
+			v[z0] <- truncnormsamp(linpred[z0],1,-Inf,0,z0.sum)
+	
+			#  Sample alpha (coefficients on basis expansion W)
+			A.inv <- solve(t(W.comb)%*%W.comb+Sigma.alpha.inv)
+			b <- t(W.comb)%*%(v-X.comb%*%beta)
+			alpha <- A.inv%*%b+t(chol(A.inv))%*%matrix(rnorm(qW),qW,1)
+			
+			#  Sample beta (coefficients on covariates influencing haul-out probability)
+			A.inv <- solve(t(X.comb)%*%X.comb+Sigma.beta.inv)
+		  	b <- t(X.comb)%*%(v-W.comb%*%alpha)  # +mu.beta%*%Sigma.beta.inv
+		  	beta <- A.inv%*%b+t(chol(A.inv))%*%matrix(rnorm(qX),qX,1)
+	
+		    # Sample z(t) (prediction of haul-out indicator variable for times t)
+			linpred <- X.comb%*%beta+W.comb%*%alpha
+			p <- pnorm(linpred[1:T,])
+			p.tmp1 <- p*dnorm(s[,1],mu.0[h.idx,1],sigma,log=FALSE)*
+				dnorm(s[,2],mu.0[h.idx,2],sigma,log=FALSE)
+			p.tmp2 <- (1-p)*dnorm(s[,1],mu.0[h.idx,1],sd.tmp,log=FALSE)*
+				dnorm(s[,2],mu.0[h.idx,2],sd.tmp,log=FALSE)
+			p.tmp <- p.tmp1/(p.tmp1+p.tmp2)
+			z <- rbinom(T,1,p.tmp)
+			
+# z <- start$z
+		
+		###
+		### Dirichlet process parameters
+		### Note: sampling order matters here. Cluster parameters must be 
+		### sampled in same order as pie, i.e., sorted by decreasing membership
+		###
+	
+			# Update follows the blocked Gibbs sampler of Ishwaran and James (2001)
+			# and Gelman et al. (2014), Section 23.3
+			
+		   	### Sample h.t (cluster assignment indicator)
+# browser()
+		   	h.idx <- sapply(1:T,function(x) sample(samp.cls,1,
+				prob=pie*(dnorm(s[x,1],mu.0[samp.cls,1],sigma)*
+				dnorm(s[x,2],mu.0[samp.cls,2],sigma))^z[x]*
+				(dnorm(s[x,1],mu.0[samp.cls,1],sd.tmp)*
+				dnorm(s[x,2],mu.0[samp.cls,2],sd.tmp))^(1-z[x])))
+		
+			# Tabulate cluster membership with data.table but not setdiff
+			# h.idx <- c(h.idx,1:H)
+			# dt.h.idx <- as.data.table(h.idx)
+			# dt.tab.cls <- dt.h.idx[,.N,by=h.idx]
+			# dt.tab.cls[,N:=N-1]
+			# setkey(dt.tab.cls,N)
+			# n.cls <- dt.tab.cls[N>0,.N]
+			# idx.cls <- rev(dt.tab.cls[N>0,h.idx])
+			# samp.cls <- rev(dt.tab.cls[,h.idx])  # order of decreasing membership
+		
+			# Tabulate cluster membership with data.table and setdiff	
+			# dt.h.idx <- as.data.table(h.idx)
+			# dt.tab.cls <- dt.h.idx[,.N,by=h.idx]
+			# setkey(dt.tab.cls,N)
+			# n.cls <- dt.tab.cls[,.N]
+			# idx.cls <- rev(dt.tab.cls[,h.idx])
+			# samp.cls <- c(idx.cls,setdiff(1:H,idx.cls))  # order of decreasing membership
+		
+			# Tabulate cluster membership with base functions
+			tab.cls <- table(h.idx)  # tabulate cluster membership
+			n.cls <- length(tab.cls)  # number of clusters
+			ord <- order(tab.cls,decreasing=TRUE) # sort clusters by membership
+			tab.cls <- tab.cls[ord]
+			idx.cls <- as.numeric(names(tab.cls))  # 'occupied' clusters
+			samp.cls <- c(idx.cls,setdiff(1:H,idx.cls))  # order of decreasing membership
+		  
+	 	    ### Sample pie (stick-breaking process)
+		    
+		    # Use for data.table functionality
+			# tab.cls.tmp <- c(rev(dt.tab.cls[,N]),rep(0,H-n.cls-1)) 
+			
+			# Use for base functionality
+	 	  	tab.cls.tmp <- c(tab.cls,rep(0,H-n.cls-1))  # membership in decreasing order
+			
+		    eta <- c(rbeta(H-1,1+tab.cls.tmp,a0+T-cumsum(tab.cls.tmp)),1)  # stick-breaking
+		    	# weights
+		    pie <- eta*c(1,cumprod((1-eta[-H])))  # mixture component probabilities
+	
+		    ### Sample a0 (concentration parameter); See Gelman section 23.3
+	       
+	    	a0 <- rgamma(1,priors$r+H-1,priors$q-sum(log(1-eta[-H])))  
+# a0 <- start$a0
+
+		###
+	    ### Sample mu.0 (true location of occupied clusters)
+		### Note: sampling order does not matter here
+	    ###
+		   
+		# Use for data.table functionality
+		# mu.0.tmp <- t(sapply(idx.cls,function(x)  # proposals for mu.0
+			# get.mu.0(x,dt.h.idx[1:T,h.idx],z,s,mu,sigma,sigma.mu,S.tilde)))
+# browser()			
+		# Use for base functionality	
+		mu.0.tmp <- t(sapply(idx.cls,function(x)  # proposals for mu.0	
+			get.mu.0(x,h.idx,z,s,Sigma.inv,Sigma.mu.inv)))  
+		idx <- which(mu.0.tmp[,1]>S.tilde[1,1]&mu.0.tmp[,1]<S.tilde[2,1]&
+			mu.0.tmp[,2]>S.tilde[1,2]&mu.0.tmp[,2]<S.tilde[3,2])  # idx of mu.0 in S.tilde	
+		mu.0[idx.cls[idx],] <- mu.0.tmp[idx,]  # accept proposals in S.tilde
+		n.cls.star <- H-n.cls  # number of new clusters to propose
+		mu.0[-idx.cls,] <- cbind(runif(n.cls.star,S.tilde[1,1],S.tilde[2,1]),
+	      runif(n.cls.star,S.tilde[1,2],S.tilde[3,2]))  # update mu.0 with mu.star
+
+
+	    ###
+	    ### Sample sigma (observation error)
+	    ###
+# browser()
+	    sigma.star <- rnorm(1,sigma,tune$sigma)
+	    if(sigma.star>priors$sigma.l & sigma.star<priors$sigma.u){
+			idx <- z==1
+			mu.0.tmp <- mu.0[h.idx,]
+	    	sd.tmp.star <- sqrt(sigma.star^2+sigma.mu^2)
+	    	mh.star.sigma <- sum(dnorm(s[idx,1],mu.0.tmp[idx,1],sigma.star,log=TRUE)+
+		    	dnorm(s[idx,2],mu.0.tmp[idx,2],sigma.star,log=TRUE))#+
+		    	sum(dnorm(s[!idx,1],mu.0.tmp[!idx,1],sd.tmp.star,log=TRUE)+
+		    	dnorm(s[!idx,2],mu.0.tmp[!idx,2],sd.tmp.star,log=TRUE))
+		    mh.0.sigma <- sum(dnorm(s[idx,1],mu.0.tmp[idx,1],sigma,log=TRUE)+
+		    	dnorm(s[idx,2],mu.0.tmp[idx,2],sigma,log=TRUE))#+
+		    	sum(dnorm(s[!idx,1],mu.0.tmp[!idx,1],sd.tmp,log=TRUE)+
+		    	dnorm(s[!idx,2],mu.0.tmp[!idx,2],sd.tmp,log=TRUE))
+	    	# mh.star.sigma <- sum(sapply(1:T, function(x) 
+	      		# dmvnorm(s[x,],mu[x,],sigma.star^2*diag(2),log=TRUE)))
+	      	# mh.0.sigma <- sum(sapply(1:T, function(x) 
+	      		# dmvnorm(s[x,],mu[x,],sigma^2*diag(2),log=TRUE)))
+		    if(exp(mh.star.sigma-mh.0.sigma)>runif(1)){
+	    	    sigma <- sigma.star
+				sd.tmp <- sd.tmp.star
+	        	Sigma.inv <- solve(sigma^2*diag(2))
+		        keep$sigma <- keep$sigma+1
+	    	} 
+	    }
+# sigma <- start$sigma
+
+
+	    ###
+	    ### Sample sigma.mu (disperson around homerange center)
+	    ###
+# browser()
+	    sigma.mu.star <- rnorm(1,sigma.mu,tune$sigma.mu)
+	    if(sigma.mu.star>priors$sigma.mu.l & sigma.star<priors$sigma.mu.u){
+			idx <- which(z==0)
+			mu.0.tmp <- mu.0[h.idx[idx],]
+	    	sd.tmp.star <- sqrt(sigma^2+sigma.mu.star^2)
+		    mh.star.sigma.mu <- sum(dnorm(s[idx,1],mu.0.tmp[,1],sd.tmp.star,log=TRUE)+
+		    	dnorm(s[idx,2],mu.0.tmp[,2],sd.tmp.star,log=TRUE))
+		    mh.0.sigma.mu <- sum(dnorm(s[idx,1],mu.0.tmp[,1],sd.tmp,log=TRUE)+
+		    	dnorm(s[idx,2],mu.0.tmp[,2],sd.tmp,log=TRUE))
+		    if(exp(mh.star.sigma.mu-mh.0.sigma.mu)>runif(1)){
+	        	sigma.mu <- sigma.mu.star
+        		sd.tmp <- sd.tmp.star
+	        	Sigma.mu.inv <- solve(sigma.mu^2*diag(2))
+		        keep$sigma.mu <- keep$sigma.mu+1
+	    	} 
+	    }
+# sigma.mu <- start$sigma.mu
+	
+		###
+		###  Save samples 
+		###
+		
+		# Use with data.table functionality
+		# h.idx.save[,k] <- dt.h.idx[1:T,h.idx]
+		# h.save[,,k] <- mu.0[dt.h.idx[1:T,h.idx],]
+		
+		# Use with base functionality
+		h.idx.save[,k] <- h.idx 
+		h.save[,,k] <- mu.0[h.idx,]
+		mu.0.save[,,k] <- mu.0
+		a0.save[k] <- a0    
+		sigma.save[k] <- sigma
+		sigma.mu.save[k] <- sigma.mu
+		alpha.save[k,] <- alpha
+		beta.save[k,] <- beta
+		v.save[,k] <- v
+		z.save[,k] <- z
+		n.cls.save[k] <- n.cls
+	}
+  
+	###
+	### Write output
+	###
+	  
+	keep$sigma <- keep$sigma/n.mcmc
+	keep$sigma.mu <- keep$sigma.mu/n.mcmc
+	cat(paste("\nsigma acceptance rate:",round(keep$sigma,2))) 
+	cat(paste("\nsigma.mu acceptance rate:",round(keep$sigma.mu,2))) 
+	cat(paste("\nTotal time elapsed:",round(difftime(Sys.time(),t.start,units="mins"),2)))
+	list(h.idx=h.idx.save,h=h.save,mu.0=mu.0.save,beta=beta.save,alpha=alpha.save,
+		a0=a0.save,sigma=sigma.save,sigma.mu=sigma.mu.save,z=z.save,v=v.save,
+	  	n.cls=n.cls.save,keep=keep,n.mcmc=n.mcmc)
+}
--- haulouts.sim.R
+++ haulouts.sim.R
@@ -1,5 +1,7 @@
 ##################################################################################
 ### Simulate 2-dimensional haul out data using a Dirichlet process mixture 
+### Developed for application with real harbor seal data using rasters, 
+### mixture t distribution, etc.
 ##################################################################################
 
 rm(list=ls())
@@ -14,10 +16,9 @@ library(splines)
 ### Define 2-dimensional rectangular support for mu, the true harbor seal locations
 ###
 
-S.bar <- cbind(c(-10,-0.5,-0.5,-10,-10),c(0,0,10,10,0))  # complement of S, i.e., the land domain
-n.haulout <- 1000
-S.tilde <- cbind(max(S.bar[,1]),  # support of haul out process
-	seq(min(S.bar[,2]),max(S.bar[,2]),length.out=n.haulout)) 
+S.bar <- cbind(c(-10,-0.5,-0.5,-10,-10),c(0,0,20,20,0))  # complement of S, i.e., the land domain
+S.tilde <- cbind(c(max(S.bar[,1]),0,0,max(S.bar[,1]),max(S.bar[,1])),S.bar[,2])  # support 
+	# of haul out process
 S <- cbind(c(max(S.bar[,1]),10,10,max(S.bar[,1]),max(S.bar[,1])),S.bar[,2])  # support of
 	# movement process (marine and haul-out environments)
 
@@ -27,51 +28,21 @@ S <- cbind(c(max(S.bar[,1]),10,10,max(S.bar[,1]),max(S.bar[,1])),S.bar[,2])  # s
 ###
 
 T <- 500  # number of locations to simulate
-theta <- 2.0  # concentration parameter
+n <- 500  # number of wet/dry observations to simulate
+a0 <- 1.0  # concentration parameter
 H <- 25  # maximum number of clusters for truncation approximation
-
-# Resource selection strength for haul-out sites
-U <- cbind(1,S.tilde[,2],sin(S.tilde[,2]))  # design matrix for haul-out site RSF
-U[,-1] <- scale(U[,-1])
-gamma <- c(-1,0.025,0.5)  # Haul-out site selection coefficients
-
-U <- cbind(1,sin(5*S.tilde[,2]))  # design matrix for haul-out site RSF
-U[,-1] <- scale(U[,-1])
-gamma <- c(0,3)  # Haul-out site selection coefficients
-
-# U <- cbind(1,rep(c(0,1),50))
-# gamma <- c(0,2)  # Haul-out site selection coefficients
-
-qU <- ncol(U)
-selection <- exp(U%*%gamma)
-plot(S.tilde[,2],selection,type="l")
-
-# idx <- order(selection,decreasing=TRUE)
-# plot(selection[idx],type="l")
-# selection[-c(1:4)] <- 0
-
-mu.0 <- sample(1:n.haulout,H,replace=TRUE,prob=selection)
-mu.0 <- S.tilde[mu.0,]
-length(unique(mu.0[,2]))==H
-eta <- c(rbeta(H-1,1,theta),1)  # stick-breaking weights
+mu.0 <- cbind(runif(H,min(S.tilde[,1]),max(S.tilde[,1])),
+	runif(H,min(S.tilde[,2]),max(S.tilde[,2])))  # clusters randomly drawn from S.tilde
+eta <- c(rbeta(H-1,1,a0),1)  # stick-breaking weights
 pie <- eta*c(1,cumprod((1-eta[-H])))  # probability mass
-h.match <- sample(1:H,T,replace=TRUE,prob=pie)  # latent cluster assignments
-h <- mu.0[h.match,]  # latent clusters
-
-plot(0,0,xlim=c(min(S.bar[,1]),max(S[,1])),ylim=range(S.bar[,2]),
-	pch="",yaxt="n",xaxt="n",xlab="",ylab="")
-polygon(x=S.bar[,1],y=S.bar[,2],col="gray45")
-polygon(x=S[,1],y=S[,2],col="gray85")
-points(S.tilde,pch=19,cex=0.25)
-points(mu.0)
-points(h,col=2,pch=19,cex=0.5)
-
+h.idx <- sample(1:H,T,replace=TRUE,prob=pie)  # latent cluster assignments
+h <- mu.0[h.idx,]  # latent clusters
 
 ###
 ### Simulate wet/dry status for T telemetry locations (s) and T SEA records (y)
 ###
 
-time <- c(0,cumsum(rgamma(T*2-1,shape=1.1,scale=2)))  # time covariate
+time <- c(0,cumsum(rgamma(T+n-1,shape=1.1,scale=2)))  # time covariate
 hr <- ((time)-24*floor(time/24))
 day <- ceiling(time/24)
 X <- cbind(1,day,hr)  # design matrix for telemetry locations and SEA data
@@ -90,7 +61,7 @@ p <- pnorm(X%*%beta+trend)  # probability of being hauled-out
 hist(p);summary(p)
 
 # Simulate wet/dry status for telemetry locations (s) and SEA data (y)
-s.idx <- sort(sample(1:(T*2),T))  # subset times between telemetry locs (s) and SEA data (y)
+s.idx <- sort(sample(1:(T+n),T))  # subset times between telemetry locs (s) and SEA data (y)
 z <- rbinom(T,1,p[s.idx])  # Haul-out indicator variable for s: 1=hauled-out, 0=at-sea
 table(z)
 y <- rbinom(T,1,p[-s.idx])  # Haul-out indicator variable for y: 1=hauled-out, 0=at-sea
@@ -118,7 +89,7 @@ qW <- ncol(W)
 ###
 
 # Simulate true locations
-sigma.mu <- 1  # dispersion about haul-out for at-sea locations
+sigma.mu <- 2  # dispersion about haul-out for at-sea locations
 mu <- matrix(0,T,2)
 mu[z==1,] <- h[z==1,]
 mu[z==0,] <- cbind(rtnorm(T-sum(z),h[z==0,1],sigma.mu,lower=S[1,1],upper=S[2,1]),
@@ -127,15 +98,15 @@ mu[z==0,] <- cbind(rtnorm(T-sum(z),h[z==0,1],sigma.mu,lower=S[1,1],upper=S[2,1])
 # Simulate observed locations
 sigma <- 0.5 # Observation error
 s <- mu
-s <- s+rnorm(T*2,0,sigma) # Athetadd error to true locations
+s <- s+rnorm(T*2,0,sigma) # Add error to true locations
 
 # Plot support
 b <- 3*c(-sigma,sigma) # Plot buffer for errors
 plot(0,0,xlim=c(min(S.bar[,1]),max(S[,1]))+b,ylim=range(S.bar[,2])+b,pch="",yaxt="n",xaxt="n",xlab="",ylab="")
 polygon(x=S.bar[,1],y=S.bar[,2],col="gray45")
 polygon(x=S[,1],y=S[,2],col="gray85")
-points(S.tilde,pch=19,cex=0.25)
-
+polygon(x=S.tilde[,1],y=S.tilde[,2],angle=45,density=5)
+ 
 # Plot true and observed locations
 points(mu,pch=19,cex=0.25,col=z+1) # All true locations
 points(s,col=z+1,pch=3,cex=0.5) # Observed locations
@@ -148,21 +119,24 @@ points(mu[z==1,],pch=19,col=rgb(1,1,1,0.6),cex=0.5) # Haul out locations
 ###
 
 # Fit model using blocked Gibbs sampler 
-source("/Users/brost/Documents/git/haulouts/haulout.dp.mixture.3.mcmc.R")
-start <- list(theta=theta,h=h,z=z,p=p,#h=fitted(kmeans(s,rpois(1,10))),
-  sigma=sigma,sigma.mu=sigma.mu,pie=pie,beta=beta,gamma=gamma)  # rdirichlet(1,rep(1/H,H))) 
-priors <- list(H=H,r=4,q=2,sigma.l=0,sigma.u=5,sigma.mu.l=0,sigma.mu.u=5,
-	sigma.beta=5,mu.gamma=rep(0,qU),sigma.gamma=5)
-tune <- list(sigma=0.05,sigma.mu=0.2,mu.0=2,gamma=2.0)
+source("/Users/brost/Documents/git/haulouts/haulouts.1.mcmc.R")
+start <- list(a0=a0,h=h,z=z,p=p,#h=fitted(kmeans(s,rpois(1,10))),
+  sigma=sigma,sigma.mu=sigma.mu,pie=pie,beta=beta)  # rdirichlet(1,rep(1/H,H))) 
+priors <- list(H=H,r=4,q=2,sigma.l=0,sigma.u=5,sigma.mu.l=0,sigma.mu.u=5,sigma.beta=1)
+tune <- list(sigma=0.05,sigma.mu=0.25,a0=0.25)
 # hist(rgamma(1000,4,2))
 # hist(rgamma(1000,5,2.5))
-out1 <- haulout.dpmixture.3.mcmc(s,y,X[s.idx,],X[-s.idx,],W[s.idx,],W[-s.idx,],
-	S,S.tilde,U,sigma.alpha=2,priors=priors,tune=tune,start=start,n.mcmc=2000)
-matplot(out1$gamma[idx,-1],type="l"); abline(h=gamma[-1],col=1:3,lty=2) 
+out1 <- haulouts.1.mcmc(s,y,X[s.idx,],X[-s.idx,],W[s.idx,],W[-s.idx,],
+	S,S.tilde,sigma.alpha=2,priors=priors,tune=tune,start=start,n.mcmc=20000)
 
-mod <- out1
+mod <- out1 
+# mod <- out2
+# mod <- out3
+# mod <- out4
+# dev.new()
+# idx <- 1:100
+idx <- 1:1000
 idx <- 1:2000
-idx <- 1:10000
 idx <- 1:20000
 
 # True clusters
@@ -170,48 +144,31 @@ b <- 3*c(-sigma,sigma) # Plot buffer for errors
 plot(0,0,xlim=c(min(S.bar[,1]),max(S[,1]))+b,ylim=range(S.bar[,2])+b,pch="",yaxt="n",xaxt="n",xlab="",ylab="")
 polygon(x=S.bar[,1],y=S.bar[,2],col="gray45")
 polygon(x=S[,1],y=S[,2],col="gray85")
-# polygon(x=S.tilde[,1],y=S.tilde[,2],angle=45,density=5)
-points(mod$h[,1,idx],mod$h[,2,idx],pch=19,cex=0.5,col=rgb(1,0,0,0.001))
+polygon(x=S.tilde[,1],y=S.tilde[,2],angle=45,density=5)
+points(mod$h[,1,idx],mod$h[,2,idx],pch=19,cex=0.5,col=rgb(0,0,0,0.0025))
 points(h,pch=1,cex=1,col=rgb(1,0,0,1))
 points(s,pch=19,cex=0.2,col=3)
 
-plot(jitter(mod$h[,1,idx]),jitter(mod$h[,2,idx]),
-	pch=19,cex=0.5,col=rgb(0,0,0,0.0025),asp=TRUE,ylim=range(S.tilde))
-points(h,pch=1,cex=1,col=rgb(1,0,0,1))
-points(s,pch=19,cex=0.2,col=3)
-points(S.tilde+cbind(rep(-1.5,n.haulout),0),cex=selection/max(selection),pch=19)
-
-which(s[,1] < (-1.5))
-pt.idx <- 280
-points(mod$h[pt.idx,1,idx],mod$h[pt.idx,2,idx],pch=19,cex=0.2,col=rgb(1,1,0,0.5))
+pt.idx <- 137
+points(mod$h[pt.idx,1,idx],mod$h[pt.idx,2,idx],pch=19,cex=0.2,col=rgb(1,1,0,0.25))
 points(mu[pt.idx,1],mu[pt.idx,2],pch=19,cex=0.75)
 points(s[pt.idx,1],s[pt.idx,2],pch=19,col=2)
 table(mod$z[pt.idx,idx])
 z[pt.idx]
 
 # Haul-out probability covariates
-matplot(mod$beta[idx,-1],type="l"); abline(h=beta,col=1:3,lty=2) 
-beta.hat <- apply(out1$beta[idx,],2,mean)
-beta.quant <- t(apply(out1$beta[idx,],2,quantile,c(0.025,0.975)))
+matplot(mod$beta[idx,],type="l"); abline(h=beta,col=1:3,lty=2) 
+beta.hat <- apply(mod$beta[idx,],2,mean)
+beta.quant <- t(apply(mod$beta[idx,],2,quantile,c(0.025,0.975)))
 plot(beta.hat,pch=19,col=rgb(0,0,0,0.25),ylim=c(range(beta.quant)))
 abline(h=0,col=2,lty=2)
 segments(1:qX,beta.quant[,1],1:qX,beta.quant[,2],col="lightgrey")
 points(beta.hat,pch=19,col=rgb(0,0,0,0.25))
 points(beta,pch=19)
 
-# Haul-out selection covariates
-matplot(mod$gamma[idx,-1],type="l"); abline(h=gamma[-1],col=1:3,lty=2) 
-gamma.hat <- apply(out1$gamma[idx,],2,mean)
-gamma.quant <- t(apply(out1$gamma[idx,],2,quantile,c(0.025,0.975)))
-plot(gamma.hat,pch=19,col=rgb(0,0,0,0.25),ylim=c(range(gamma.quant)))
-abline(h=0,col=2,lty=2)
-segments(1:qU,gamma.quant[,1],1:qU,gamma.quant[,2],col="lightgrey")
-points(gamma.hat,pch=19,col=rgb(0,0,0,0.25))
-points(gamma,pch=19)
-
 # Inference on alpha
-alpha.hat <- apply(out1$alpha[idx,],2,mean)
-alpha.quant <- t(apply(out1$alpha[idx,],2,quantile,c(0.025,0.975)))
+alpha.hat <- apply(mod$alpha[idx,],2,mean)
+alpha.quant <- t(apply(mod$alpha[idx,],2,quantile,c(0.025,0.975)))
 plot(alpha.hat,pch=19,col=rgb(0,0,0,0.25),ylim=c(range(alpha.quant)))
 abline(h=0,col=2,lty=2)
 segments(1:qW,alpha.quant[,1],1:qW,alpha.quant[,2],col="lightgrey")
@@ -224,13 +181,13 @@ plot(alpha.hat,pch=19,col=rgb(0,0,0,0.25),ylim=c(range(alpha.quant)))
 lines(alpha.hat,col=rgb(0,0,0,0.25))
 abline(h=0,col=2,lty=2)
 
-# Concentration parameter (theta) and number of clusters
-hist(mod$theta[idx],breaks=100);abline(v=theta,col=2,lty=2) 
-mean(mod$theta[idx])*log(T)
+# Concentration parameter
+hist(mod$a0[idx],breaks=100);abline(v=a0,col=2,lty=2) 
+mean(mod$a0[idx])*log(T)
 
+# Modeled number of clusters
 # n.cls <- apply(mod$h[,,idx],3,function(x) nrow(unique(x)))
-plot(mod$n.cls,type="l")
-abline(h=nrow(unique(h)),col=2,lty=2)  # true number of clusters  
+plot(mod$n.cls,type="l");abline(h=nrow(unique(h)),col=2,lty=2)  # true number of clusters  
 barplot(table(mod$n.cls)) 
 
 # Observation error
@@ -243,32 +200,26 @@ mean(mod$sigma.mu[idx])
 
 # Inference on z: latent haul-out indicator variable for telemetry locations
 # Note: estimation of z (z.hat) is based on covariates and location of telemetry 
-# observations, whereas v is based on covariates alone.
+# observations, whereas u is based on covariates alone.
 z.hat <- apply(mod$z[,idx],1,sum)/(length(idx))
 boxplot(z.hat~z,ylim=c(0,1))
 plot(s[,1],z.hat,ylim=c(0,1),col=z+1)
-abline(v=S.tilde[1,1],col=3,lty=2)
-plot(z,col=3,pch=19,cex=0.5)
-points(z.hat,pch=19,cex=0.5,col=rgb(0,0,0,0.25))
 
 v <- apply(mod$beta[idx,],1,function(x) X[s.idx,]%*%x)+
 	apply(mod$alpha[idx,],1,function(x) W[s.idx,]%*%x)
 v.inv <- matrix(pnorm(v),,T,byrow=TRUE)
 v.inv.mean <- apply(v.inv,2,mean)
-plot(v.inv.mean,z.hat)
 v.inv.quant <- t(apply(v.inv,2,quantile,c(0.025,0.975)))
 plot(v.inv.mean,pch=19,col=rgb(0,0,0,0.25),ylim=c(0,1))
 segments(1:T,v.inv.quant[,1],1:T,v.inv.quant[,2],col=rgb(0,0,0,0.15))
 abline(h=0.5,col=2,lty=2)
 points(z,col=3,pch=19,cex=0.5)
-points(z.hat,pch=19,cex=0.25,col=1)
+points(z.hat,pch=19,cex=0.25)
 
 # Inference for y
-v.tilde.inv <- matrix(pnorm(mod$v[,idx]),,T,byrow=TRUE)
+v.tilde.inv <- matrix(pnorm(mod$v[-(1:T),idx]),,n,byrow=TRUE)
 v.tilde.inv.mean <- apply(v.tilde.inv,2,mean)
 v.tilde.inv.quant <- t(apply(v.tilde.inv,2,quantile,c(0.025,0.975)))
 plot(v.tilde.inv.mean,pch=19,col=rgb(0,0,0,0.25),ylim=c(0,1))
-segments(1:T,v.tilde.inv.quant[,1],1:T,v.tilde.inv.quant[,2],col=rgb(0,0,0,0.15))
+segments(1:n,v.tilde.inv.quant[,1],1:n,v.tilde.inv.quant[,2],col=rgb(0,0,0,0.15))
 points(y,col=4,pch=19,cex=0.5)
-
-
